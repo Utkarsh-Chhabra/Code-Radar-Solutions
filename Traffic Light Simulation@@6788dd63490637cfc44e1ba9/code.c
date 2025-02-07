7bfd4e709ceb8031c8263cada2329022fec0f455
Utkarsh-Chhabra <utkarsh0643.becse24@chitkara.edu.in>
2025-02-07 22:21:48 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     char ch;
-    scanf("%c",ch);
+    scanf("%c",&ch);
     if(ch="R"){
         printf("Stop");
     }
@@ -16,5 +16,4 @@ int main() {
     }
 
     return 0;
-
 }
